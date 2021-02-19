c10976201fe5a3426ee7d7991475908d4a12c36a
mehzan07 <61779364+mehzan07@users.noreply.github.com>
2021-02-19 17:03:02 +0100

    Update Program.cs
    improving of implementation to show correct max traffic for each node and sorted array according to the nodes.

--- CityTraffic/Program.cs
+++ CityTraffic/Program.cs
@@ -1,126 +1,123 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Collections;
-//Obs! this program should be investigated the output is not that expected.
-
-
-
-
-
+/// <summary>
+/// inplementation of max traffic for each node (city) in a graph.
+/// </summary>
 
 class MainClass
 {
+    static void Main()
+    {
+        // string[] stingArray = new string[] { "1:[5]", "3:[5]", "4:[5]", "5:[1,4,3,2]", "2:[5,15,7]", "7:[2,8]", "8:[7,38]", "15:[2]", "38:[8]" };
+       // Example of graph string as input:
+       //  1:[5] 3:[5] 4:[5] 5:[1,4,3,2] 2:[5,15,7] 7:[2,8] 8:[7,38] 15:[2] 38:[8]
+         Console.WriteLine("Please give a graph string as input: example 1:[5] 5[1]...");
+        string stingArray2 = Console.ReadLine();
+        string[] stingArray = stingArray2.Split(' ').ToArray();
+        Console.WriteLine();
+        Console.WriteLine("Max traffic for each node in the graph as a sorted array is as following:");
+        Console.WriteLine(CityTraffic(stingArray));
+
+        // if input is: 1:[5] 3:[5] 4:[5] 5:[1,4,3,2] 2:[5,15,7] 7:[2,8] 8:[7,38] 15:[2] 38:[8]
+        // Then Output should be: 1:60,2:53,3:60,4:60,5:55,7:46,8:38,15:55,38:32  which is correct.
+    }
 
     public static string CityTraffic(string[] strArr)
     {
-
-        // code goes here  
         string maxtraffics = string.Empty;
-        string firstNode = string.Empty;
         int NrOfCities = strArr.Length;
         int Node;
         string[] neighbors;
-      
 
-        // go thew all nodes (Cities) in the graph
+        try
+        {
+        // go thew all nodes (Cities) in the graph and find max trafic for each node according max traffic of neighbors. 
         for (int i = 0; i < NrOfCities; i++)
         {
-             // find Node and neighbores for this
-            Node = Int32.Parse(strArr[i].ToCharArray().ElementAt(0).ToString());
+            // find Node and neighbores for this
+            int index = strArr[i].ToString().IndexOf(':');
+            Node = Int32.Parse(strArr[i].ToString().Substring(0, index));
             neighbors = findNeighbors(Node, strArr);
-            int neighborsTraffic = GetNeighborsTraffic(Node, neighbors, strArr);
+
+            int nodeTraffic = GetMaxNodeTraffic(Node, neighbors, strArr);
             if (i == NrOfCities - 1)
-                maxtraffics += Node.ToString() + ":" + neighborsTraffic.ToString();
+                maxtraffics += Node.ToString() + ":" + nodeTraffic.ToString();
             else
-           maxtraffics += Node.ToString() + ":" + neighborsTraffic.ToString() + ",";
+                maxtraffics += Node.ToString() + ":" + nodeTraffic.ToString() + ",";
         }
         // Sort and remove the duplicates.
-        var listofstr = maxtraffics.Split(',');
-        var sortlistofstr = listofstr.OrderBy(x => x.ToCharArray().ElementAt(0)).Distinct().ToList();
+         var listofstr = maxtraffics.Split(',');
+        var sortlistofstr = listofstr.OrderBy(x => Int32.Parse(x.ToString().Split(':')[0])).Distinct().ToList();
         maxtraffics = string.Join(",", sortlistofstr.ToArray());
-        return maxtraffics;
+        }
+        catch(Exception ex)
+        {
+            string msg = ex.Message.ToString();
+           Console.WriteLine("Exception from method: CityTraffic, messge: " + msg);
+        }
 
+        return maxtraffics;
+    }
+
+    // Get max traffic for neighbors of a node (city) in the Graph.
+    public static int GetMaxNodeTraffic(int node, string[] neighbors, string[] strArr)
+    {
+        int maxNodeTraffic = 0;
+        string[] tNeighbors = null;
+        List<int> lNeTr = new List<int>();
+        try
+        {
+        for (int j = 0; j < neighbors.Length; j++)
+        {
+            int tNode = Int32.Parse(neighbors[j]);
+            //find Neighbors of tNode
+            tNeighbors = findNeighbors(tNode, strArr);
+            //if node exist in the neighbors remove node from the list.
+            var list = new List<string>(tNeighbors);
+            list.Remove(node.ToString());
+            tNeighbors = list.ToArray();
+            maxNodeTraffic = tNode + GetMaxNodeTraffic(tNode, tNeighbors, strArr);
+            lNeTr.Add(maxNodeTraffic);
+        }
+        }
+        catch (Exception ex)
+        {
+            string msg = ex.Message.ToString();
+            Console.WriteLine("Exception from method:GetMaxNodeTraffic and message: " + msg);
+        }
+        if (lNeTr.Count != 0)
+            maxNodeTraffic = lNeTr.Max();
+        else
+            maxNodeTraffic = 0;
+        return maxNodeTraffic;
     }
 
     // returns neighbor of a node in the strArr.
     public static string[] findNeighbors(int node, string[] strArr)
     {
         string[] neighbors = null;
-        for (int i = 0; i < strArr.Length; i++)
-            if (strArr[i].Contains(node.ToString() + ":"))
-            {
-                string tempstr = strArr[i].Substring(2).Replace(":", "").Replace("[", "").Replace("]", "");
-                neighbors = tempstr.Split(',');
-                return neighbors;
-            }
-        return neighbors;
-    }
-
-    // get max traffic for neighbors of a node (city)
-    public static int GetNeighborsTraffic(int node, string[] neighbors, string[] strArr)
-    {
-        int NeighborsTr = 0;
-        string[] tNeighbors = null;
-
-        for (int j = 0; j < neighbors.Length; j++)
+        try
         {
-            int tNode = Int32.Parse(neighbors[j]);
-            //find number of tNode
-            tNeighbors = findNeighbors(tNode, strArr);
-            //if node exist in the neighbors remove node from the list.
-            var list = new List<string>(tNeighbors);
-            list.Remove(node.ToString());
-            tNeighbors = list.ToArray();
-            //Get traffic from this node (city).
-            NeighborsTr += System.Math.Max(tNode, GetNeighborsTraffic(tNode, tNeighbors, strArr));  // gives stack overflow
-        }
-            return NeighborsTr;
-    }
-
-    public static int getMaxtraffic(int j, string[] strAr)
-    {
-        int maxtr = 0;
-        int NrOfCities = strAr.Length;
-
-        string CityAndpop;
-
-        string[] CityNeighbors;
-
-        //find j+ ":" in the strArr
-        for (int k = 0; k < NrOfCities; k++)
-        {
-            if (strAr[k].Contains(j.ToString() + ":"))
+            for (int i = 0; i < strArr.Length; i++)
             {
-                CityAndpop = strAr[k];
-                CityNeighbors = new[] { CityAndpop.Substring(2) };
-                string tempstr = CityAndpop.Substring(2).Replace("[", "").Replace("]", "");
-                CityNeighbors = tempstr.Split(",");
-                for (int kk = 0; kk < CityNeighbors.Length; kk++)
+                if (strArr[i].Contains(node.ToString() + ":"))
                 {
-                    int node = Int32.Parse(string.Join("", CityNeighbors[kk]));
-                    maxtr += System.Math.Max(node, getMaxtraffic(node, strAr));
+                    string tempstr = strArr[i].Substring(2).Replace(":", "").Replace("[", "").Replace("]", "");
+                    neighbors = tempstr.Split(',');
+                    return neighbors;
                 }
             }
         }
-        return maxtr;
+
+        catch (Exception ex)
+        {
+            string msg = ex.Message.ToString();
+            Console.WriteLine("Exception from method: findNeighbors, messge: " + msg);
+        }
+        return neighbors;
     }
 
-
-
-
-  
-    static void Main()
-            {
-                // keep this function call here
-                string[] stingArray = {"1:[5]", "4:[5]", "3:[5]", "5:[1,4,3,2]", "2:[5,15,7]", "7:[2,8]", "8:[7,38]", "15:[2]", "38:[8]"};
-        // Console.WriteLine(CityTraffic(Console.ReadLine()));
-        Console.WriteLine(CityTraffic(stingArray));
-
-        // the output is not correct it should be investigated
-        // out put should be: 1:82,2:53,3:80,4:79,5:70,7:46,8:38,15:68,38:45.
-        // just now the output is the following which is not correct even 1 is comming two times
-        // 1:60,4:57,3:58,5:61,2:61,7:61,8:61,1:60,3:58
-    }
 
 }
